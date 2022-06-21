6a66f3bd60ad32742310d1cd9403858c98acdf6b
Alejandro <alejandromunozc@gmail.com>
2022-06-21 00:12:46 -0500

    Start project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Resistor Color Code</h1>
+    </div>
+  )
+}
+
+export default App

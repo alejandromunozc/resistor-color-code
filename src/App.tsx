64649c14b831d7a774774a3be3f7d4c6c0ce58b3
Alejandro <alejandromunozc@gmail.com>
2022-06-21 04:24:54 -0500

    Add Tailwind Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import SelectMode from './components/SelectMode'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SelectMode />
       <h1>Resistor Color Code</h1>
     </div>
   )
